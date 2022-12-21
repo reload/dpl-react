c9ba3665a2eaa36b57bb5fc28679e70750dd691d
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-12-21 20:53:07 +0100

    Skip material list requests

--- src/components/button-favourite/button-favourite.tsx
+++ src/components/button-favourite/button-favourite.tsx
@@ -1,9 +1,6 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { IconFavourite } from "../icon-favourite/icon-favourite";
-import {
-  removeItem,
-  useHasItem
-} from "../../core/material-list-api/material-list";
+import { removeItem } from "../../core/material-list-api/material-list";
 import { useText } from "../../core/utils/text";
 import { Pid, WorkId } from "../../core/utils/types/ids";
 
@@ -21,27 +18,27 @@ const ButtonFavourite: React.FC<ButtonFavouriteProps> = ({
 }) => {
   const [fillState, setFillState] = useState<boolean>(false);
   const t = useText();
-  const { mutate } = useHasItem();
+  // const { mutate } = useHasItem();
 
-  useEffect(() => {
-    mutate(
-      {
-        listId: "default",
-        itemId: id
-      },
-      {
-        onSuccess: () => {
-          setFillState(true);
-        },
-        // The material list service will return response code 404 when a
-        // material is not on the patrons list. This is interpreted as an
-        // error by our client. Consequently we set
-        onError: () => {
-          setFillState(false);
-        }
-      }
-    );
-  }, [id, mutate]);
+  // useEffect(() => {
+  //   mutate(
+  //     {
+  //       listId: "default",
+  //       itemId: id
+  //     },
+  //     {
+  //       onSuccess: () => {
+  //         setFillState(true);
+  //       },
+  //       // The material list service will return response code 404 when a
+  //       // material is not on the patrons list. This is interpreted as an
+  //       // error by our client. Consequently we set
+  //       onError: () => {
+  //         setFillState(false);
+  //       }
+  //     }
+  //   );
+  // }, [id, mutate]);
 
   const handleClick = useCallback(
     (e: React.MouseEvent<HTMLButtonElement>) => {
