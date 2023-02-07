867ad4e7868c4f58dbea157d64db68ca923e27f8
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-02-07 09:39:10 +0100

    Remove app_testimng build from vs

--- app_testing/build/src/core/utils/helpers/date.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const getCurrentUnixTime = () => Math.floor(Date.now() / 1000);
-exports.default = getCurrentUnixTime;
--- app_testing/build/src/core/utils/helpers/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userIsAnonymous = void 0;
-const token_1 = require("../../token");
-const userIsAnonymous = () => {
-    return !(0, token_1.hasToken)("user");
-};
-exports.userIsAnonymous = userIsAnonymous;
-exports.default = {};
