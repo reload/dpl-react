501742b7aa844f10505ccf70ead8b2c9f5ca6678
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-02-08 20:36:54 +0100

    Make sure that tested elements are in viewport
    After lazy loading was introduced we sometimes need to scroll down for
    some elements to be visible/available.

--- src/components/material/MaterialDescription.tsx
+++ src/components/material/MaterialDescription.tsx
@@ -35,7 +35,11 @@ const MaterialDescription: React.FC<MaterialDescriptionProps> = ({ work }) => {
   const { fictionNonfiction } = work;
 
   return (
-    <section ref={itemRef} className="material-description">
+    <section
+      ref={itemRef}
+      className="material-description"
+      data-cy="material-description"
+    >
       {showItem && (
         <>
           <h2 className="text-header-h4 pb-24">
